e63c4bff175031f55e9132c459fbd7d137426ace
Huy Luong <huy2005luong@gmail.com>
2026-04-16 15:07:26 +0700

    Update server port and cors for production

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,15 +3,30 @@ import router from './routes/task.routes.js'
 import databaseService from './services/database.services.js'
 import { userRouter } from './routes/user.routes.js'
 import cors from 'cors'
-const app= express()
-const port = 5001
+import dotenv from 'dotenv' 
+
+dotenv.config()
+
+const app = express()
+
+const port = process.env.PORT || 5001 
+
 app.use(express.json())
-app.use(cors())
-app.use('/api/tasks' ,router)
+
+
+app.use(cors({
+  origin: process.env.CLIENT_URL || '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  credentials: true 
+}))
+
+app.use('/api/tasks', router)
 app.use('/api/users', userRouter)
+
 databaseService.connect().then(() => {
     app.listen(port, () => {
-        console.log(`Server is running accurately at: http://localhost:${port}`)
+     
+        console.log(`Server is running at port: ${port}`)
     })
 }).catch((err: any) => {
     console.error("Critical error starting server:", err)
