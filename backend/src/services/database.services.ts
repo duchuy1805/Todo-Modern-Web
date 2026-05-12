fefa6cce7915be3c36b1e2aa6b45f070fe5bb1f1
Lương Đức Huy <huy2005luong@gmail.com>
2026-05-13 00:30:27 +0700

    feat: add Dockerfile and docker-compose

--- backend/src/services/database.services.ts
+++ backend/src/services/database.services.ts
@@ -1,32 +1,43 @@
 import 'dotenv/config'
 import { MongoClient, Db, Collection } from 'mongodb'
-const uri= process.env.MONGODB_URI
+
 class DatabaseService {
     private client: MongoClient
     private db: Db
+
     constructor(){
-        this.client= new MongoClient(uri as string)
-        this.db=this.client.db(process.env.DB_NAME)
+        const uri = process.env.MONGODB_URI || "mongodb://mongo:27017/todox"
+        const dbName = process.env.DB_NAME || "todox"
+        
+        const cleanUri = uri.replace(/^"|"$/g, '').trim()
+        
+        console.log("Đang thử kết nối MongoDB tới URI:", cleanUri)
+
+        this.client = new MongoClient(cleanUri)
+        this.db = this.client.db(dbName)
     }
+
     async connect() {
-    try {
-      await this.client.connect()
-      await this.db.command({ ping: 1 })
-      console.log(`Pinged your deployment. You successfully connected to MongoDB!`)
-    } catch (err) {
-      console.error("Error in connecting to database: ", err)
-      throw err
+        try {
+            await this.client.connect()
+            await this.db.command({ ping: 1 })
+            console.log(`Pinged your deployment. You successfully connected to MongoDB!`)
+        } catch (err) {
+            console.error("Error in connecting to database: ", err)
+            throw err
+        }
     }
-  }
+
     get tasks(): Collection<any>{
-      return this.db.collection('task')
+        return this.db.collection('task')
     }
     get users(): Collection<any>{
-      return this.db.collection('user')
+        return this.db.collection('user')
     }
     get refresh_token(): Collection<any>{
-      return this.db.collection('refresh_token')
+        return this.db.collection('refresh_token')
     }
 }
-const databaseService= new DatabaseService()
+
+const databaseService = new DatabaseService()
 export default databaseService
